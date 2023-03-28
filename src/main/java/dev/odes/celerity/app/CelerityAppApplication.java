1220288fc045212c480c7d9f0e0331775e2e3f0e
odedev <odedev@outlook.com>
2023-03-28 11:39:22 +0800

    init

---
+++ src/main/java/dev/odes/celerity/app/CelerityAppApplication.java
@@ -0,0 +1,13 @@
+package dev.odes.celerity.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelerityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelerityAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/odes/celerity/app/CelerityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.odes.celerity.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelerityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
